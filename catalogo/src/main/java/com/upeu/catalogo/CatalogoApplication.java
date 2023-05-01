c237c230c1e05bf76443429c07d76754408bd995
IVANMAMANI2003 <ivanyomm.2003@gmail.com>
2023-05-01 12:05:28 -0500

    springdoc

--- catalogo/src/main/java/com/upeu/catalogo/CatalogoApplication.java
+++ catalogo/src/main/java/com/upeu/catalogo/CatalogoApplication.java
@@ -2,13 +2,26 @@ package com.upeu.catalogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class CatalogoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogoApplication.class, args);
-		System.out.println("hi!!");
 	}
-
+	@Bean
+    public OpenAPI custoOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("OPEN API MICROSERVICIOS CATALOGO")
+                .version("0.0.1")
+                .description("servicio web catalogo")
+                .termsOfService("http://swagger.io/terms")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+        );
+    }
 }
